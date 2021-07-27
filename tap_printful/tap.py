f3da5fb0321a747ba3d18a9450649e1f5a395f87
Ivan Kuznetsov <me@jeiwan.ru>
2021-07-27 15:25:26 +0300

    Define config keys

--- tap_printful/tap.py
+++ tap_printful/tap.py
@@ -18,12 +18,9 @@ class Tapprintful(Tap):
     """printful tap class."""
     name = "tap-printful"
 
-    # TODO: Update this section with the actual config values you expect:
     config_jsonschema = th.PropertiesList(
-        th.Property("auth_token", th.StringType, required=True),
-        th.Property("project_ids", th.ArrayType(th.StringType), required=True),
+        th.Property("api_key", th.StringType, required=True),
         th.Property("start_date", th.DateTimeType),
-        th.Property("api_url", th.StringType, default="https://api.mysample.com"),
     ).to_dict()
 
     def discover_streams(self) -> List[Stream]:
